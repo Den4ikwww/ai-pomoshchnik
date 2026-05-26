cd83d2fe1417ab416a1f38d32a0d7f9b7bc95f2b
Den4ikwww <ivden00@mail.ru>
2026-05-26 12:20:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'ai-helper-v2'; // Обновил версию, чтобы сработало принудительно
+const CACHE_NAME = 'ai-helper-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
